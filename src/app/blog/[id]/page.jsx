b7c2ceea79fd472a2a5bdacad1e1a9be4d58b71a
HJL <1439715703@qq.com>
2025-07-22 16:26:06 +0800

    finish UI design

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import styles from './page.module.css'
+import Image from 'next/image'
 
 const BlogPost = () => {
   return (
-    <div>BlogPost</div>
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Description</p>
+          <div className={styles.author}>
+            <Image
+              src="/illustration.png"
+              alt="test"
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span>John Doe</span>
+          </div>
+        </div>
+
+        <div className={styles.imageContainer}>
+          <Image
+            src="/illustration.png"
+            alt="test"
+            fill
+            className={styles.image}
+          />
+        </div>
+      </div>
+
+      <div className={styles.content}>
+        <div className={styles.text}>
+        texttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttext
+        </div>
+      </div>
+    </div>
   )
 }
 
