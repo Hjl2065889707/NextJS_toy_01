5230840531d85b7fe2a8db87141104af99532fc0
HJL <1439715703@qq.com>
2025-07-22 16:54:53 +0800

    DarkModeToggle UI

---
+++ src/components/DarkModeToggle/DarkModeToggle.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './DarkModeToggle.module.css'
+
+const DarkModeToggle = () => {
+    const mode = "dark"
+    return (
+        <div className={styles.container}>
+            <div className={styles.icon}>🌙</div>
+            <div className={styles.icon}>☀️</div>
+            <div className={styles.ball}
+                style={mode === "light" ? { left: "4px" } : { right: "4px" }}
+            />
+        </div>
+    )
+}
+
+export default DarkModeToggle
