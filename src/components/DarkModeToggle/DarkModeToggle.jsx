66eca92a4a8e35235e707041c1d8ce157d68bebf
HJL <1439715703@qq.com>
2025-07-23 20:16:32 +0800

    finish dark mode

--- src/components/DarkModeToggle/DarkModeToggle.jsx
+++ src/components/DarkModeToggle/DarkModeToggle.jsx
@@ -1,10 +1,14 @@
+"use client"
+
 import React from 'react'
 import styles from './DarkModeToggle.module.css'
+import { ThemeContext } from '../../../context/ThemeContext';
+import { useContext } from 'react';
 
 const DarkModeToggle = () => {
-    const mode = "dark"
+    const { mode, toggle } = useContext(ThemeContext);
     return (
-        <div className={styles.container}>
+        <div className={styles.container} onClick={toggle}>
             <div className={styles.icon}>🌙</div>
             <div className={styles.icon}>☀️</div>
             <div className={styles.ball}
